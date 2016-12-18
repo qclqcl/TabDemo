ba54d2b7c6bc4cc9e94f6ebdce7e8a041ea71746
qclqcl <18813131203@163.com>
2016-12-18 16:51:35 +0800

    Add download satinfo from internet.

---
+++ src/com/andyidea/tabdemo/ImageService.java
@@ -0,0 +1,22 @@
+package com.andyidea.tabdemo;
+
+import java.io.IOException;  
+import java.io.InputStream;  
+import java.net.HttpURLConnection;  
+import java.net.URL;  
+  
+import com.andyidea.tabdemo.StreamTool;
+  
+public class ImageService {  
+      
+    public static byte[] getImage(String path) throws IOException {  
+        URL url = new URL(path);  
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();  
+        conn.setRequestMethod("GET");   //设置请求方法为GET  
+        conn.setReadTimeout(5*1000);    //设置请求过时时间为5秒  
+        InputStream inputStream = conn.getInputStream();   //通过输入流获得图片数据  
+        byte[] data = StreamTool.readInputStream(inputStream);     //获得图片的二进制数据  
+        return data;  
+          
+    }  
+}
